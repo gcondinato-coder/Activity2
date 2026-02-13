524559ec1187ab1cb631d0ee9114e1dea3aba447
gcondinato-coder <gcondinato@spamast.edu.ph>
2026-02-13 09:49:57 +0545

    Bot 1st phase

---
+++ TriviaBot.js
@@ -0,0 +1,16 @@
+Console.log("Hello! I'm you coding fun fact guide!");
+let botName = "KUdoo";
+let botLocation = "Salvacion";
+let favoriteLanguage = "JavaScript";
+
+console.log(`Myname ${botName} and my location ${botLocation}`);
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+console.log(codingFact);
+codingFact = favoriteLanguage + " is one of the most popular programming languages in the world.";
+console.log(`${codingFact}`);
+codingFact = favoriteLanguage + ", you can build websites and even mobile apps!";
+console.log(`${codingFact}`);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
